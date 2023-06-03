8ba7f854df2a7d56b0cf29d6cba25b193c61c3df
AndreaOrtenzi <andre.orto@tiscali.it>
2023-06-03 19:36:20 +0200

    First draft without Jion, gating vars and forcing term

--- CardiacElectrophysiology.hpp
+++ CardiacElectrophysiology.hpp
@@ -1,6 +1,3 @@
-#ifndef CARDIAC_ELECTROPHYSIOLOGY_HPP
-#define CARDIAC_ELECTROPHYSIOLOGY_HPP
-
 #include <deal.II/base/conditional_ostream.h>
 #include <deal.II/base/quadrature_lib.h>
 
@@ -13,6 +10,7 @@
 #include <deal.II/fe/fe_system.h>
 #include <deal.II/fe/fe_values.h>
 #include <deal.II/fe/fe_values_extractors.h>
+#include <deal.II/fe/mapping_fe.h>
 
 #include <deal.II/grid/grid_in.h>
 
@@ -29,214 +27,366 @@
 
 using namespace dealii;
 
-// Ma fare delle classi friends con i metodi relativi ai due problemi??
+#define H(x,x0) x>x0
 
-// Class representing the cardiac electrophysiology problem.
-class CardiacElectrophysiology
+
+// Class representing the Bueno-Orovio model of the heart
+class BuenoOrovio
 {
 public:
-  // Physical dimension of the diffusion problem (PDE)(3D).
-  static constexpr unsigned int dim = 3;
+    // Physical dimension (1D, 2D, 3D)
+    static constexpr unsigned int dim = 3;
 
-  // Physical dimension of the gating problem (ODE)(3D). (v, w, s)
-  static constexpr unsigned int dim_gating = 4;
 
-  // Function for the D coefficient (diffusion coefficient).
-  class DiffusionCoefficient : public Function<dim>
-  {
-  public:
-    // define value method
-  };
+    // Functions. ///////////////////////////////////////////////////////////////
 
-  // Function for the ionic current.
-  class IonicCurrent : public Function<dim>
-  {
-  public:
-    // define value method
-  };
+    // Function for the mu coefficient.
+    class FunctionMu : public Function<dim>
+    {
+    public:
+        virtual double
+        value(const Point<dim> & /*p*/,
+              const unsigned int /*component*/ = 0) const override
+        {
+            return 1.171;
+        }
+    };
 
-  // Function for the forcing term (applied current).
-  class AppliedCurrent : public Function<dim>
-  {
-  public:
-    // define value method
-  };
+    class Heaviside : public Function<dim>
+    {
+    public:
+        virtual double value(const Point<dim> &p, const unsigned int /*component*/ = 0) const override
+        {
+            u(p);
+            return std::sin(5 * M_PI * get_time()) * std::sin(2 * M_PI * p[0]) *
+                    std::sin(3 * M_PI * p[0]) * std::sin(4 * M_PI * p[2]);
+        }
 
-  // Neumann boundary conditions.
-  class FunctionH : public Function<dim>
-  {
-  public:
-    // Constructor.
-    FunctionH()
+        // virtual Tensor<1, dim>
+        // gradient(const Point<dim> &p,
+        //         const unsigned int /*component*/ = 0) const override
+        // {
+        // Tensor<1, dim> result;
+
+        // // duex / dx
+        // result[0] = 2 * M_PI * std::sin(5 * M_PI * get_time()) *
+        //             std::cos(2 * M_PI * p[0]) * std::sin(3 * M_PI * p[1]) *
+        //             std::sin(4 * M_PI * p[2]);
+
+        // // duex / dy
+        // result[1] = 3 * M_PI * std::sin(5 * M_PI * get_time()) *
+        //             std::sin(2 * M_PI * p[0]) * std::cos(3 * M_PI * p[1]) *
+        //             std::sin(4 * M_PI * p[2]);
+
+        // // duex / dz
+        // result[2] = 4 * M_PI * std::sin(5 * M_PI * get_time()) *
+        //             std::sin(2 * M_PI * p[0]) * std::sin(3 * M_PI * p[1]) *
+        //             std::cos(4 * M_PI * p[2]);
+
+        // return result;
+        // }
+    };
+
+    // Function for the forcing term.
+    class ForcingTerm : public Function<dim>
+    {
+    public:
+        virtual double
+        value(const Point<dim> &p,
+              const unsigned int /*component*/ = 0) const override
+        {
+            return 0; // get_time();    
+        }
+    };
+
+    // Functions for the initial conditions
+    class FunctionU0 : public Function<dim>
+    {
+    public:
+        virtual double
+        value(const Point<dim> &p,
+            const unsigned int /*component*/ = 0) const override
+        {
+        return 0;
+        }
+    };
+
+    class FunctionV0 : public Function<dim>
+    {
+    public:
+        virtual double
+        value(const Point<dim> &p,
+            const unsigned int /*component*/ = 0) const override
+        {
+        return 1;
+        }
+    };
+
+    class FunctionW0 : public Function<dim>
+    {
+    public:
+        virtual double
+        value(const Point<dim> &p,
+            const unsigned int /*component*/ = 0) const override
+        {
+        return 1;
+        }
+    };
+
+    class FunctionS0 : public Function<dim>
+    {
+    public:
+        virtual double
+        value(const Point<dim> &p,
+            const unsigned int /*component*/ = 0) const override
+        {
+        return 0;
+        }
+    };
+
+    // Constructor. We provide the final time, time step Delta t and theta method
+    // parameter as constructor arguments.
+    BuenoOrovio(const unsigned int &N_,
+         const unsigned int &r_,
+         const double &T_,
+         const double &deltat_
+         // add tissue choice
+         )
+        : mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD)), mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)), pcout(std::cout, mpi_rank == 0), T(T_), N(N_), r(r_), deltat(deltat_), mesh(MPI_COMM_WORLD)
     {}
 
-    // Evaluation:
-    virtual double
-    value(const Point<dim> &p, const unsigned int /*component*/ = 0) const
-    {
-      return 0.;
-    }
-  };
+    // Initialization.
+    void setup();
 
-  // Function dv/dt.
-  class DerivativeV : public Function<dim>
-  {
-  public:
-    virtual double
-    value(const Point<dim> &p,
-          const unsigned int /*component*/ = 0) const override
-    {
-      double heaviside = ((p[0] - theta) > 0);
-      return (1 - heaviside) * (v_inf - p[1]) / tau_minus -
-             heaviside * p[1] / tau_plus;
-    }
+    // Solve the problem.
+    void solve();
 
-  protected:
-    const double theta     = 0.;
-    const double v_inf     = 0.;
-    const double tau_minus = 0.;
-    const double tau_plus  = 0.;
-  };
-
-  // Function dw/dt.
-  class DerivativeW : public Function<dim>
-  {
-  public:
-    virtual double
-    value(const Point<dim> &p,
-          const unsigned int /*component*/ = 0) const override
-    {
-      double heaviside = ((p[0] - theta) > 0);
-      return (1 - heaviside) * (w_inf - p[2]) / tau_minus -
-             heaviside * p[2] / tau_plus;
-    }
-
-  protected:
-    const double theta     = 0.;
-    const double w_inf     = 0.;
-    const double tau_minus = 0.;
-    const double tau_plus  = 0.;
-  };
-
-  // Function ds/dt.
-  class DerivativeS : public Function<dim>
-  {
-  public:
-    virtual double
-    value(const Point<dim> &p,
-          const unsigned int /*component*/ = 0) const override
-    {
-      return ((1 + std::tanh(k_s * (p[0] - u_s))) / 2 - p[3]) / tau_s;
-    }
-
-  protected:
-    const double k_s   = 0.;
-    const double u_s   = 0.;
-    const double tau_s = 0.;
-  };
-
-  // Constructor.
-  CardiacElectrophysiology(const unsigned int &N_, const unsigned int &r_)
-    : mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD))
-    , mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD))
-    , pcout(std::cout, mpi_rank == 0)
-    , N(N_)
-    , r(r_)
-    , mesh(MPI_COMM_WORLD)
-  {}
-
-  // Initialization.
-  void
-  setup();
-
-  // Assemble the diffusion and the gating problems.
-  void
-  assemble_systems();
-
-  // Solve the tangent problem.
-  void
-  solve_diffusion_system();
-
-  // Solve the gating problem.
-  void
-  solve_gating_system();
-
-  // Output.
-  void
-  output() const;
+    
 
 protected:
-  // MPI parallel. /////////////////////////////////////////////////////////////
 
-  // Number of MPI processes.
-  const unsigned int mpi_size;
+    // Model's parameters: //////////////
+    struct TissueParameters { 
+        double u_0;
+        double u_u;
+        double theta_v;
+        double theta_w;
+        double theta_v_min;
+        double theta_0;
+        double tau_v1_min;
+        double tau_v2_min;
+        double tau_v_plus;
+        double tau_w1_min;
+        double tau_w2_min;
+        double k_w_min;
+        double u_w_min;
+        double tau_w_plus;
+        double tau_fi;
+        double tau_o1;
+        double tau_o2;
+        double tau_so1;
+        double tau_so2;
+        double k_so;
+        double u_so;
+        double tau_s1;
+        double tau_s2;
+        double k_s;
+        double u_s;
+        double tau_si;
+        double tau_w_inf;
+        double w_inf_star;
+    };
 
-  // This MPI process.
-  const unsigned int mpi_rank;
 
-  // Parallel output stream.
-  ConditionalOStream pcout;
+    constexpr TissueParameters Epicardium {
+        .u_0 = 0.0,
+        .u_u = 1.55,
+        .theta_v = 0.3,
+        .theta_w = 0.13,
+        .theta_v_min = 0.006,
+        .theta_0 = 0.006,
+        .tau_v1_min = 60e-3,
+        .tau_v2_min = 1150e-3,
+        .tau_v_plus = 1.4506e-3,
+        .tau_w1_min = 60e-3,
+        .tau_w2_min = 15e-3,
+        .k_w_min = 65,
+        .u_w_min = 0.03,
+        .tau_w_plus = 200e-3,
+        .tau_fi = 0.11e-3,
+        .tau_o1 = 400e-3,
+        .tau_o2 = 6e-3,
+        .tau_so1 = 30.0181e-3,
+        .tau_so2 = 0.9957e-3,
+        .k_so = 2.0458,
+        .u_so = 0.65,
+        .tau_s1 = 2.7342e-3,
+        .tau_s2 = 16e-3,
+        .k_s = 2.0994,
+        .u_s = 0.9087,
+        .tau_si = 1.8875e-3,
+        .tau_w_inf = 0.07,
+        .w_inf_star = 0.94
+    };
+    constexpr TissueParameters Endocardium {
+        .u_0 = 0.0,
+        .u_u = 1.56,
+        .theta_v = 0.3,
+        .theta_w = 0.13,
+        .theta_v_min = 0.2,
+        .theta_0 = 0.006,
+        .tau_v1_min = 75e-3,
+        .tau_v2_min = 10e-3,
+        .tau_v_plus = 1.4506e-3,
+        .tau_w1_min = 6e-3,
+        .tau_w2_min = 140e-3,
+        .k_w_min = 200,
+        .u_w_min = 0.0016,
+        .tau_w_plus = 280e-3,
+        .tau_fi = 0.1e-3,
+        .tau_o1 = 470e-3,
+        .tau_o2 = 6e-3,
+        .tau_so1 = 40e-3,
+        .tau_so2 = 1.2e-3,
+        .k_so = 2,
+        .u_so = 0.65,
+        .tau_s1 = 2.7342e-3,
+        .tau_s2 = 2e-3,
+        .k_s = 2.0994,
+        .u_s = 0.9087,
+        .tau_si = 2.9013e-3,
+        .tau_w_inf = 0.0273,
+        .w_inf_star = 0.78
+    };
+    constexpr TissueParameters Myocardium {
+        .u_0 = 0.0,
+        .u_u = 1.61,
+        .theta_v = 0.3,
+        .theta_w = 0.13,
+        .theta_v_min = 0.1,
+        .theta_0 = 0.005,
+        .tau_v1_min = 80e-3,
+        .tau_v2_min = 1.4506e-3,
+        .tau_v_plus = 1.4506e-3,
+        .tau_w1_min = 70e-3,
+        .tau_w2_min = 8e-3,
+        .k_w_min = 200,
+        .u_w_min = 0.0016,
+        .tau_w_plus = 280e-3,
+        .tau_fi = 0.078e-3,
+        .tau_o1 = 410e-3,
+        .tau_o2 = 7e-3,
+        .tau_so1 = 91e-3,
+        .tau_so2 = 0.8e-3,
+        .k_so = 2.1,
+        .u_so = 0.6,
+        .tau_s1 = 2.7342e-3,
+        .tau_s2 = 4e-3,
+        .k_s = 2.0994,
+        .u_s = 0.9087,
+        .tau_si = 3.3849e-3,
+        .tau_w_inf = 0.01,
+        .w_inf_star = 0.5
+    };
 
-  // Diffusion problem definition. ///////////////////////////////////////////////////////
+    static constexpr double D = 1.171 //+-0.221
+    
 
-  // Diffusion coefficient.
-  DiffusionCoefficient D;
+    // FE methods: //////////////////////
 
-  // Ionic current.
-  IonicCurrent I_ion;
+    // Assemble the mass and stiffness matrices.
+    void assemble_matrices();
 
-  // Applied current.
-  AppliedCurrent I_app;
+    // Assemble the right-hand side of the problem.
+    void assemble_rhs(const double &time);
 
-  // Neumann boundary condition.
-  FunctionH function_h;
+    // Solve the problem for one time step.
+    void solve_time_step();
 
-  // Gating problem definition. /////////////////////////////////////////////////
-  
-  // Derivative of v wrt time.
-  DerivativeV derivative_v;
+    // Output.
+    void output(const unsigned int &time_step, const double &time) const;
 
-  // Derivative of w wrt time.
-  DerivativeW derivative_w;
+    // MPI parallel. /////////////////////////////////////////////////////////////
 
-  // Derivative of s wrt time.
-  DerivativeS derivative_s;
+    // Number of MPI processes.
+    const unsigned int mpi_size;
 
-  // Discretization. ///////////////////////////////////////////////////////////
+    // This MPI process.
+    const unsigned int mpi_rank;
 
-  // Mesh refinement.
-  const unsigned int N;
+    // Parallel output stream.
+    ConditionalOStream pcout;
 
-  // Polynomial degree.
-  const unsigned int r;
+    // Problem definition. ///////////////////////////////////////////////////////
 
-  // Mesh.
-  parallel::fullydistributed::Triangulation<dim> mesh;
+    // mu coefficient.
+    FunctionMu mu;
 
-  // Finite element space.
-  std::unique_ptr<FiniteElement<dim>> fe;
+    // Forcing term.
+    ForcingTerm forcing_term;
 
-  // Quadrature formula.
-  std::unique_ptr<Quadrature<dim>> quadrature;
+    // initial conditions
+    FunctionU0 u0; 
+    FunctionW0 w0;
+    FunctionV0 v0;
+    FunctionS0 s0;
 
-  // DoF handler.
-  DoFHandler<dim> dof_handler;
+    // Exact solution.
+    ExactSolution exact_solution;
 
-  // DoFs owned by current process.
-  IndexSet locally_owned_dofs;
+    // Current time.
+    double time;
 
-  // DoFs relevant to the current process (including ghost DoFs).
-  IndexSet locally_relevant_dofs;
+    // Final time.
+    const double T;
 
-  // Jacobian matrix.
-  TrilinosWrappers::SparseMatrix system_matrix;
+    // Discretization. ///////////////////////////////////////////////////////////
 
-  // Residual vector.
-  TrilinosWrappers::MPI::Vector system_rhs;
+    // Mesh refinement.
+    const unsigned int N;
 
-  // System solution (without ghost elements).
-  TrilinosWrappers::MPI::Vector solution_owned;
+    // Polynomial degree.
+    const unsigned int r;
 
-  // System solution (including ghost elements).
-  TrilinosWrappers::MPI::Vector solution;
+    // Time step.
+    const double deltat;
+
+    // Mesh.
+    parallel::fullydistributed::Triangulation<dim> mesh;
+
+    // Finite element space.
+    std::unique_ptr<FiniteElement<dim>> fe;
+
+    // Quadrature formula.
+    std::unique_ptr<Quadrature<dim>> quadrature;
+
+    // DoF handler.
+    DoFHandler<dim> dof_handler;
+
+    // DoFs owned by current process.
+    IndexSet locally_owned_dofs;
+
+    // DoFs relevant to the current process (including ghost DoFs).
+    IndexSet locally_relevant_dofs;
+
+    // Mass matrix M / deltat.
+    TrilinosWrappers::SparseMatrix mass_matrix;
+
+    // Stiffness matrix A.
+    TrilinosWrappers::SparseMatrix stiffness_matrix;
+
+    // Matrix on the left-hand side (M / deltat + theta A).
+    TrilinosWrappers::SparseMatrix lhs_matrix;
+
+    // Matrix on the right-hand side (M / deltat - (1 - theta) A).
+    TrilinosWrappers::SparseMatrix rhs_matrix;
+
+    // Right-hand side vector in the linear system.
+    TrilinosWrappers::MPI::Vector system_rhs;
+
+    // System solution (without ghost elements).
+    TrilinosWrappers::MPI::Vector solution_owned;
+
+    // System solution (including ghost elements).
+    TrilinosWrappers::MPI::Vector solution;
 };
